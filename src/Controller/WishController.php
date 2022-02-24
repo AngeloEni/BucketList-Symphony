87e72f0b1751e59d061bba33cb49c4bff97e047d
AngeloEni <angelo.fernandes2021@campus-eni.fr>
2022-02-24 15:59:50 +0100

    ajout d'un formulaire

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -3,48 +3,50 @@
 namespace App\Controller;
 
 use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WishController extends AbstractController
 {
-     /**
+    /**
      * @Route("/wish", name="wish")
      */
     public function wish(WishRepository $req): Response
     {
-        $lst = $req->findBy([],["dateCreated"=>"DESC"]);
-      
-        $wish = ['DANSER sur LA PLAGE','FAIRE DU BATEAU','FAIRE DU SOUS MARIN','VACANCES EN FAMILLE']; 
-       
-        
+        $lst = $req->findBy([], ["dateCreated" => "DESC"]);
+
+        $wish = ['DANSER sur LA PLAGE', 'FAIRE DU BATEAU', 'FAIRE DU SOUS MARIN', 'VACANCES EN FAMILLE'];
+
+
         return $this->render('wish/wish.html.twig', [
             'titre' => 'wish',
             'list' => $wish,
             'req' => $lst,
         ]);
-    } 
+    }
 
-      /**
+    /**
      * @Route("/detail/{id}", name="detail")
      */
     public function detail(Wish $wish): Response
     {
-       
-        
-       
 
-         return $this->render('wish/detail.html.twig', [
+
+
+
+        return $this->render('wish/detail.html.twig', [
             'titre' => 'detail',
             'wish' => $wish,
         ]);
     }
 
-       /**
+    /**
      * @Route("/about/", name="about")
      */
     public function about(): Response
@@ -53,7 +55,7 @@ class WishController extends AbstractController
             'titre' => 'Page about',
         ]);
     }
-       /**
+    /**
      * @Route("/", name="home")
      */
     public function hello(): Response
@@ -64,43 +66,88 @@ class WishController extends AbstractController
     }
 
 
-     /**
+    /**
      * @Route("/delete/{id}", name="delete")
      */
-    public function add(EntityManagerInterface $em, Wish $wish): Response
+    public function delete(EntityManagerInterface $em, Wish $wish): Response
     {
-        
-    
+
+
         //supprimer
         $em->remove($wish);
-        $em->flush();//Save execute la requète Sql
-        
-        
-        return $this->redirectToRoute('wish');
+        $em->flush(); //Save execute la requète Sql
 
-       
+
+        return $this->redirectToRoute('wish');
     }
 
-    
 
-     /**
+
+    /**
      * @Route("/add", name="add")
      */
-    public function delete(EntityManagerInterface $em): Response
+    public function add(EntityManagerInterface $em): Response
     {
-        
+
         $w = new Wish();
         $w->setTitle("add test");
         $w->setDescription("lolololo");
         $w->setAuthor("l'author de roro");
-        //persist uniquement creation
+        $w->setIsPublished(true);
+        $w->setDatecreated(new \DateTime()); // l'antislash signifie ici que \DateTime est une classe native 
+        //persist uniquement creation // pas de persiste quand on est dans l'update 
         $em->persist($w);
-        $em->flush();//Save execute la requète Sql
-        
-        
+        $em->flush(); //Save execute la requète Sql
+
+
         return $this->redirectToRoute('wish');
+    }
+
+      /**
+     * @Route("/addby", name="addby")
+     */
+    public function added(Request $req, EntityManagerInterface $em): Response
+    {
+
+       
+       $w = new Wish();
+       $w->setTitle( $req->get('title'));
+       $w->setDescription( $req->get('Description'));
+       $w->setAuthor($req->get('author'));
+       $w->setIsPublished($req->get('radios'));
+       $w->setDatecreated(new \DateTime());
+       
+       $em->persist($w);
+       $em->flush();
+
+       return $this->redirectToRoute('wish');
 
     }
 
-    
+     /**
+     * @Route("/addeasy", name="addeasy")
+     */
+    public function addedeasy(Request $req, EntityManagerInterface $em): Response
+    {
+
+       $wish = new wish();
+       $form = $this->createForm(WishType::class,$wish);
+
+       $form->handleRequest($req);
+
+       if($form->isSubmitted()){
+
+            $em->persist($wish);
+            $em->flush();
+            return $this->redirectToRoute('home');
+
+       }
+       
+       return $this->render('wish/ajouter.html.twig',['formulaire' => $form->createView()]);
+     
+
+      
+
+    }
+
 }
